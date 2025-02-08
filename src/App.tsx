873dab72950fcb5aa544c15b66e9b40bf8cfcee5
AlexaVas <alexandra.vasinova@gmail.com>
2025-02-08 14:22:06 +0100

    edited ts config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 
 import Canvas from './experience/Canvas.jsx'
 import PaginationUI from './components/PaginationUI.js';
-import { useRingConfigurator } from './store/useRingConfigurator.js';
 import Loader from './components/Loader.js';
 export default function App() {
-const textureLoading = useRingConfigurator((state)=>state.textureLoading)
+
   return (
     <>
     <section className='w-full h-full min-h-fit flex md:flex-row flex-col  '>
